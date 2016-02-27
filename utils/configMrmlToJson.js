1a92a831ab37f0aca8ed12317ab3f8889686ff78
Valentin Demeusy <valentin.demeusy@gmail.com>
2016-02-27 02:04:34 -0500

    initial commit
    Signed-off-by: Valentin Demeusy <valentin.demeusy@gmail.com>

---
+++ utils/configMrmlToJson.js
@@ -0,0 +1,7 @@
+//all the path given relatively to mrmlToJson.js script
+module.exports = {
+    mrmlFileLocation : "../../atlas/Abdominal_Atlas_2008.mrml",
+    colorTableFileLocation : "",
+    vtkFilesDirectory : "../../atlas/",
+    jsonResultFileName : "atlasStructure.json"
+}
