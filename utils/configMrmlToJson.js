32bd6882c8990c2ed4b29af40583800882e8b810
Valentin Demeusy <valentin.demeusy@gmail.com>
2016-03-09 02:04:31 -0500

    enh : update data and submodules
    Signed-off-by: Valentin Demeusy <valentin.demeusy@gmail.com>

--- utils/configMrmlToJson.js
+++ utils/configMrmlToJson.js
@@ -1,7 +1,19 @@
 //all the path given relatively to mrmlToJson.js script
 module.exports = {
-    mrmlFileLocation : "../../atlas/Abdominal_Atlas_2008.mrml",
-    colorTableFileLocation : "",
-    vtkFilesDirectory : "../../atlas/",
-    jsonResultFileName : "atlasStructure.json"
+    mrmlFileLocation : "../../AbdominalAtlas-2015Sept-Slicer4-4Version.mrml",
+    colorTableFileLocation : "../../Data/AbdominalAtlasColors.ctbl",
+    vtkFilesDirectory : "../../Data/",
+    jsonResultFileName : "../atlasStructure.json",
+    header : {
+        "@type": "header",
+        "species": "human",
+        "organ": "Abdominal",
+        "name" : "The SPL/NAC Abdominal Atlas",
+        "license" : "?",
+        "citation" : "?",
+        "version" : "1",
+        "contact" : "https://github.com/stity/spl-abdominal-atlas",
+        "comment" : "",
+        "coordinate_system" : "self defined"
+    }
 }
